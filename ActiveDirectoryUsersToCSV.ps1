b2813e72edd77967bff79871e1e7c88e4e9bf912
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-02-01 02:39:56 -0800

    Add files via upload

---
+++ ActiveDirectoryUsersToCSV.ps1
@@ -0,0 +1,4 @@
+﻿Import-module ActiveDirectory
+get-aduser -filter * -Properties displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title |
+select displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title |
+export-csv ($env:USERPROFILE + "\Documents\ActiveDirectoryUsers.csv") -NoTypeInformation
