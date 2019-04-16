eefc89d921129170c164bdda447c9865a1e19e83
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-04-16 03:55:31 -0700

    Add files via upload

---
+++ ActiveDirectoryUsersToCSV-DisabledUsersOnly.ps1
@@ -0,0 +1,5 @@
+﻿Import-module ActiveDirectory
+get-aduser -filter * -Properties displayName,givenName,sn,distinguishedname,mail,mailnickname,title |
+Where { $_.Enabled -eq $False} |
+select displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title |
+export-csv ($env:USERPROFILE + "\Documents\ActiveDirectoryDisabledUsers.csv") -NoTypeInformation
---
+++ ActiveDirectoryUsersToCSV-EnabledUsersOnly.ps1
@@ -0,0 +1,5 @@
+﻿Import-module ActiveDirectory
+get-aduser -filter * -Properties displayName,givenName,sn,distinguishedname,mail,mailnickname,title |
+Where { $_.Enabled -eq $True} |
+select displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title |
+export-csv ($env:USERPROFILE + "\Documents\ActiveDirectoryEnabledUsers.csv") -NoTypeInformation
