9338869123e6e7e7ff327a0427465ad335a5749a
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-05-07 23:02:32 -0700

    Update ActiveDirectoryUsersToCSV-EnabledUsersOnly.ps1

--- ActiveDirectoryUsersToCSV-EnabledUsersOnly.ps1
+++ ActiveDirectoryUsersToCSV-EnabledUsersOnly.ps1
@@ -1,5 +1,5 @@
-﻿Import-module ActiveDirectory
-get-aduser -filter * -Properties displayName,givenName,sn,distinguishedname,mail,mailnickname,title |
+Import-module ActiveDirectory
+get-aduser -filter * -Properties displayName,givenName,sn,distinguishedname,mail,mailnickname,title,physicalDeliveryOfficeName |
 Where { $_.Enabled -eq $True} |
-select displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title |
+select displayName,Enabled,givenName,sn,distinguishedname,mail,mailnickname,title,physicalDeliveryOfficeName |
 export-csv ($env:USERPROFILE + "\Documents\ActiveDirectoryEnabledUsers.csv") -NoTypeInformation
