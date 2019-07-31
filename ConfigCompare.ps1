1e81b3215ed18cd4ae582e8d4c5b196363b03ca0
Cameron Lewis <46030732+cblewis@users.noreply.github.com>
2019-07-30 17:16:01 -0700

    Update ConfigCompare.ps1

--- ConfigCompare.ps1
+++ ConfigCompare.ps1
@@ -1,1 +1,4 @@
-Compare-Object -ReferenceObject $(Get-Content C:\Users\CameronLewis\Downloads\test.txt) -DifferenceObject $(Get-Content C:\Users\CameronLewis\Downloads\test2.txt) #| Out-File -FilePath C:\Users\CameronLewis\Downloads\results.txt
+$folder1 = Get-Content C:\Users\CameronLewis\Downloads\FILE1.txt | ForEach-Object { $_.Trim() }
+$folder2 = Get-Content C:\Users\CameronLewis\Downloads\FILE2.txt | ForEach-Object { $_.Trim() }
+
+Compare-Object $folder1 $folder2 #|Out-File -FilePath C:\Users\CameronLewis\Downloads\results.txt
